0a0bb866dedd3d121c0b1d295138c1141de1e772
DoDongssss <xchitts@gmail.com>
2023-04-18 23:58:06 +0800

    add file

---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="text-4xl">Hello World</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
